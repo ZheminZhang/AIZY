f0305153f466719e63533bb734af184783d600a0
shanzq <shanzq233@outlook.com>
2019-06-29 10:45:41 +0800

    x

--- pages/authorize/authorize.js
+++ pages/authorize/authorize.js
@@ -72,15 +72,49 @@ Page({
       }
     })
   },
-  // 加载url中的参数
+  msToDate: function (msec) {
+    let datetime = new Date(msec);
+    let year = datetime.getFullYear();
+    let month = datetime.getMonth();
+    let date = datetime.getDate();
+    let hour = datetime.getHours();
+    let minute = datetime.getMinutes();
+    let second = datetime.getSeconds();
+
+    let result1 = year +
+      '-' +
+      ((month + 1) >= 10 ? (month + 1) : '0' + (month + 1)) +
+      '-' +
+      ((date + 1) < 10 ? '0' + date : date) +
+      ' ' +
+      ((hour + 1) < 10 ? '0' + hour : hour) +
+      ':' +
+      ((minute + 1) < 10 ? '0' + minute : minute) +
+      ':' +
+      ((second + 1) < 10 ? '0' + second : second);
+
+    let result2 = year +
+      '-' +
+      ((month + 1) >= 10 ? (month + 1) : '0' + (month + 1)) +
+      '-' +
+      ((date + 1) < 10 ? '0' + date : date);
+
+    let result = {
+      hasTime: result1,
+      withoutTime: result2
+    };
+    return result;
+  },
+  // 加载url中的参数,同时完成unix转普通时间
   onLoad: function (options) {
+    console.log(this.msToDate(options.authStartTime).withoutTime);
     this.setData({
       companyName: options.companyName,
       id: options.id,
-      authStartTime: options.authStartTime,
-      authEndTime: options.authEndTime,
-      recordStartTime: options.recordStartTime,
-      recordEndTime: options.recordEndTime,
+      authStartTime: msToDate(options.authStartTime).withoutTime,
+      authEndTime: msToDate(options.authEndTime).withoutTime,
+      recordStartTime: msToDate(options.recordStartTime).withoutTime,
+      recordEndTime: msToDate(options.recordEndTime).withoutTime,
       type: options.type,
     })
   }
